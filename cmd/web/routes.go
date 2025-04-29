3b4c10704d4cbc74a1f2f79ca425a783f1adb818
Plump Albert <plumpalbert@gmail.com>
2025-04-29 09:57:17 +0300

    use custom handler for 404 status

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,13 +9,16 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	router.HandlerFunc(http.MethodGet, "/", app.IndexHandler)
 	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.SnippetViewHandler)
-	router.HandlerFunc(http.MethodGet, "/snippet/create", app.SnippetCreateHandler)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.SnippetCreateViewHandler)
 	router.HandlerFunc(http.MethodPost, "/snippet/create", app.SnippetCreateHandler)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
